f5756716f98f86fdfa8169b9c5e19ab24237cdcc
Yoonwonsuh <blacksyw@naver.com>
2022-07-26 22:50:58 +0900

    df

---
+++ src/pages/main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../components/layout/layout";
+import Header from "../components/header/header";
+
+const Main = () => {
+    return (
+        <Layout>
+            <Header />
+
+        </Layout>
+
+    );
+
+};
+
+export default Main;
